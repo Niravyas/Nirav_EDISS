93cce5daada579f6ff415952c9b1ddfa26553be8
Nirav Vyas <niravyas@yahoo.com>
2017-08-04 21:17:47 -0400

    corrected the view products query13

--- Project4/server.js
+++ Project4/server.js
@@ -412,12 +412,12 @@ app.post('/viewProducts', function (req, res) {
                          console.log("ITs coming here");
                         var obj= '{"message":"The action was successful","product":[';    
                         var result = [];
-                        var pn = "";
+                        
                         var temp ="";
                         for(var i =0; i< rows.length; i++)
                         {
                             var arrOfStr = rows[i].productName.split(",");
-                            
+                            var pn = "";
                             for(var j=0; j< arrOfStr.length; j++){
                                 pn = pn + arrOfStr[j];
                                 temp= '{"asin":"'+rows[i].asin+'","productName":"'+pn+'"}';
